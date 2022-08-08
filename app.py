7863983a45f1f72ac6f3850eeb2e6a286f60c333
Samsung <Samsung@gmail.com>
2022-08-08 18:09:54 +0530

    Login And Regster Done

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
-
+from pyisemail import is_email
 # Configure application
 app = Flask(__name__)
 
@@ -23,7 +23,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///users.db")
+db = SQL("sqlite:///stonks.db")
 
 # Make sure API key is set
 # if not os.environ.get("API_KEY"):
@@ -40,10 +40,10 @@ def after_request(response):
 
 
 @app.route("/")
-# @login_required
+@login_required
 def index():
     """Show portfolio of stocks"""
-    return render_template("index.html")
+    return render_template("dashboard.html")
 
 
 @app.route("/buy", methods=["GET", "POST"])
@@ -98,20 +98,17 @@ def login():
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
+        # Ensure username and password was submitted
+        if not request.form.get("email") or not request.form.get("password"):
+            return render_template("login.html", error="Invalid Email or password")
 
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
 
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        # Query database for email
+        rows = db.execute("SELECT * FROM users WHERE email = ?", request.form.get("email"))
 
-        # Ensure username exists and password is correct
+        # Ensure email exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return render_template("login.html", error="Invalid Email or password")
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -161,27 +158,32 @@ def quote():
 def register():
     """Register user"""
 
-    username = request.form.get("username")
-    password = request.form.get("password")
-    confirmation = request.form.get("confirmation")
 
     if(request.method == "POST"):
-        rows = db.execute("SELECT * FROM users WHERE username =?;", username)
-        if(len(rows) == 1):
-            return apology("Username Already Taken")
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
 
-        if(password != confirmation):
-            return apology("Passwords Don't Match")
+        rows_uname = db.execute("SELECT * FROM users WHERE username =?;", username)
+        rows_email = db.execute("SELECT * FROM users WHERE email =?;", email)
+        if(not username or not password or not email):
+            return render_template("register.html",error="Empty Fields")
 
-        if(not username or not password or not confirmation):
-            return apology("Empty Fields")
+        if(len(rows_uname) == 1):
+            return render_template("register.html",error="Username Already Taken")
+
+        if(len(rows_email) == 1):
+            return render_template("register.html",error="Email Already Used")
+
+        if(not is_email(email)):
+            return render_template("register.html",error="Invalid Email")
 
         hash = generate_password_hash(request.form.get("password"))
-        db.execute("INSERT INTO users (username,hash) VALUES (?,?);", username , hash)
+        db.execute("INSERT INTO users (username,email,hash) VALUES (?,?,?);", username, email, hash)
 
         row = db.execute("SELECT * FROM users WHERE username=?", username)
         session["user_id"] = row[0]["id"]
-        return redirect("/register")
+        return render_template("register.html", error="User Registered")
 
     return render_template("register.html")
 
---
+++ test.py
@@ -0,0 +1,4 @@
+from pyisemail import is_email
+
+email="who@gmail.com"
+print(is_email(email, check_dns=True))
